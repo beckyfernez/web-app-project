388a804053822580e1e7a11a5bb69f41bb5eb713
Rebecca Fernandez <97413846+beckyfernez@users.noreply.github.com>
2023-05-17 21:58:35 -0500

    Descriptions added for review_test.py

--- review_test.py
+++ review_test.py
@@ -1,5 +1,8 @@
+# This is the "review_test.py" file...
+
 from web_app.review_submit import review_upload
 
+#This tests whether a user submission is inserted correctly into the data sheet (check for one inserted row)
 def test_if_new_row_created_for_review():
     form_data_test = {"location_id":"1", "taste_score":"3", "health_score":"3", "service_score":"3","portion_score":"3"}
     assert review_upload(form_data_test) == 1
